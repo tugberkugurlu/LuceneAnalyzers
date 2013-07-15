ef1d50eb60ed96052baba762287b6dbe8b750bbd
tugberkugurlu <tugberkugurlu@gmail.com>
2013-07-15 17:33:25 +0300

    initial commit

---
+++ src/LuceneAnalyzers/TurkishLowerCaseKeywordAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using Lucene.Net.Analysis;
+using System.IO;
+
+namespace LuceneAnalyzers
+{
+    public class TurkishLowerCaseKeywordAnalyzer : Analyzer
+    {
+        public override TokenStream ReusableTokenStream(string fieldName, TextReader reader)
+        {
+            var previousTokenStream = (TutkishLowerCaseKeywordTokenizer)PreviousTokenStream;
+            if (previousTokenStream == null)
+            {
+                return TokenStream(fieldName, reader);
+            }
+
+            previousTokenStream.Reset(reader);
+            return previousTokenStream;
+        }
+
+        public override TokenStream TokenStream(string fieldName, TextReader reader)
+        {
+            return new TutkishLowerCaseKeywordTokenizer(reader);
+        }
+    }
+}
